3bcde3bef8a6656e1474e9793ba6749247061eef
cksiow <cksiow@gmail.com>
2023-11-30 23:12:07 +0800

    ticket_no_4 #comment 1. add swagger summary as documentation purpose for todo 2. change README mention swagger as interface documentation

--- src/main/java/com/cognixus/todo/controller/TodoController.java
+++ src/main/java/com/cognixus/todo/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.cognixus.todo.controller;
 
 import com.cognixus.todo.model.Todo;
 import com.cognixus.todo.service.api.TodoService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,21 +16,25 @@ public class TodoController {
     private TodoService todoService;
 
     @PostMapping()
-    public List<Todo> saveAll(@RequestBody List<Todo> users) {
-        return todoService.saveAll(users);
+    @Operation(summary = "Insert todo list data")
+    public List<Todo> saveAll(@RequestBody List<Todo> todos) {
+        return todoService.saveAll(todos);
     }
 
     @GetMapping()
+    @Operation(summary = "Get own todo list either all, pending or completed")
     public List<Todo> findByUser(@RequestParam(required = false, defaultValue = "") Boolean completed) {
         return todoService.findByUser(completed);
     }
 
     @DeleteMapping()
+    @Operation(summary = "Delete own to do data")
     public void deleteAllByIdIn(@RequestBody List<Long> ids) {
         todoService.deleteAllByIdIn(ids);
     }
 
     @PostMapping("/completed")
+    @Operation(summary = "update to do item as completed")
     public List<Todo> updateCompletedByIdIn(@RequestBody List<Long> ids) {
         todoService.updateCompletedByIdIn(ids);
         //need to let update completed committed then retrieve results
